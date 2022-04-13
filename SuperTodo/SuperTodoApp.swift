365ea57144091028d2a3646bf68bd69bdf448308
kwawrzecki <karol.wawrzecki@imagene.me>
2022-04-13 14:42:28 +0200

    Initial Commit

---
+++ SuperTodo/SuperTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperTodoApp.swift
+//  SuperTodo
+//
+//  Created by Karol Wawrzecki on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
